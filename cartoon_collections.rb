273e7b4fd213968683d819bcf06d9ea0692f9cd0
andrew kim <andrewk1216@gmail.com>
2019-08-21 02:40:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,6 @@ end
 def find_the_cheese(strings)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  cheese_types.each{|string|
+    
 end
