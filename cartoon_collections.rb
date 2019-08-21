1523df90e228305f0d505c40c66a8610548f076c
andrew kim <andrewk1216@gmail.com>
2019-08-21 02:35:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ end
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
   planeteer_calls.map {|call|
-    "#{call.capitalize}"
+    "#{call.capitalize}!"
+  }
 end
 
 def long_planeteer_calls# code an argument here
