21dbcbb464050ce7e23374d4a6937e0b99e054fb
andrew kim <andrewk1216@gmail.com>
2019-08-21 02:37:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,9 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.any?
+  calls.any? {|word|
+    word.length > 4
+  }
 end
 
 def find_the_cheese# code an argument here
